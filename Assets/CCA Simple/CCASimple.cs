a14103a6c29556f64bb7baa98077268825ecec0a
Kit MacAllister <kmacallister@secondstory.com>
2020-03-08 01:30:57 -0800

    Lots of updates, figured out 2D noise for my terrain generator

---
+++ Assets/CCA Simple/CCASimple.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using EasyButtons;
+using UnityEngine;
+
+public class CCASimple : MonoBehaviour
+{
+    // ------------------------------
+    // Primary CCA Parameters
+    // ------------------------------
+    [Header("Simple CCA Settings")]
+    
+    const int MAX_RANGE = 10;
+    [Range(1, MAX_RANGE)]
+    public int range = 1;
+
+    const int MAX_THRESHOLD = 25;
+    [Range(0, MAX_THRESHOLD)]
+    public int threshold = 3;
+
+    const int MAX_STATES = 20;
+    [Range(0, MAX_STATES)]
+    public int nstates = 3;
+
+    public bool moore;
+
+    // ------------------------------
+    // Global Parameters
+    // ------------------------------
+    [Header("Setup")]
+
+    [Range(0, 2048)]
+    public int rez = 8;
+
+    [Range(0, 50)]
+    public int stepsPerFrame = 0;
+
+    [Range(1, 50)]
+    public int stepMod = 1;
+
+    public ComputeShader cs;
+
+    public Material readMat;
+    public Material writeMat;
+    public Material outMat;
+    
+    private RenderTexture outTex;
+    private RenderTexture readTex;
+    private RenderTexture writeTex;
+
+    private int stepKernel;
+
+    // ------------------------------
+    // Start
+    // ------------------------------
+    private void Start()
+    {
+        Reset();    
+    }
+
+    // ------------------------------
+    // Update
+    // ------------------------------
+    private void Update()
+    {
+        if(Time.frameCount % stepMod == 0)
+        {
+            for (int i = 0; i < stepsPerFrame; i++)
+            {
+                Step();
+            }
+        }
+    }
+
+    // ------------------------------
+    // Reset
+    // ------------------------------
+    [Button]
+    private void Reset()
+    {
+        readTex = CreateTexture(RenderTextureFormat.RFloat);
+        writeTex = CreateTexture(RenderTextureFormat.RFloat);
+        outTex = CreateTexture(RenderTextureFormat.ARGBFloat);
+
+        stepKernel = cs.FindKernel("StepKernel");
+
+        GPUResetKernel();
+    }
+
+    private void GPUResetKernel()
+    {
+        int k = cs.FindKernel("ResetKernel");
+        cs.SetTexture(k, "writeTex", writeTex);
+
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+
+        cs.SetInt("rez", rez);
+        cs.Dispatch(k, rez / 8, rez / 8, 1);
+        SwapTex();
+    }
+
+    // ------------------------------
+    // Randomize Parameters
+    // ------------------------------
+    [Button]
+    public void RandomizeParams()
+    {
+        var rand = new System.Random();
+        range = (int) (rand.NextDouble() * (MAX_RANGE - 1)) + 1;
+        threshold = (int) (rand.NextDouble() * (MAX_THRESHOLD - 1)) + 1;
+        nstates = (int) (rand.NextDouble() * (MAX_STATES - 2)) + 2;
+        moore = rand.NextDouble() <= 0.5f;
+        
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    [Button]
+    public void SetPrimaryParams()
+    {
+        cs.SetInt("range", range);
+        cs.SetInt("threshold", threshold);
+        cs.SetInt("nstates", nstates);
+        cs.SetBool("moore", moore);
+    }
+
+    [Button]
+    public void ResetAndRandomize()
+    {
+        RandomizeParams();
+        Reset();
+    }
+
+    // ------------------------------
+    // Step
+    // ------------------------------
+    [Button]
+    public void Step()
+    {
+        cs.SetTexture(stepKernel, "readTex", readTex);
+        cs.SetTexture(stepKernel, "writeTex", writeTex);
+        cs.SetTexture(stepKernel, "outTex", outTex);
+
+        cs.Dispatch(stepKernel, rez / 8, rez / 8, 1);
+
+        SwapTex();
+
+        writeMat.SetTexture("_UnlitColorMap", readTex);
+        readMat.SetTexture("_UnlitColorMap", writeTex);
+        outMat.SetTexture("_UnlitColorMap", outTex);
+    }
+    
+    // ------------------------------
+    // Helper Functions
+    // ------------------------------
+    private void SwapTex()
+    {
+        RenderTexture tmp = readTex;
+        readTex = writeTex;
+        writeTex = tmp;
+    }
+    
+    protected RenderTexture CreateTexture(RenderTextureFormat format)
+    {
+        RenderTexture texture = new RenderTexture(rez, rez, 1, format);
+        texture.enableRandomWrite = true;
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Repeat;
+        texture.useMipMap = false;
+        texture.Create();
+        return texture;
+    }
+}
---
+++ Assets/Erosion/GPUErosion.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using EasyButtons;
+using UnityEngine;
+
+public class GPUErosion : MonoBehaviour
+{
+    // ------------------------------
+    // Primary CCA Parameters
+    // ------------------------------
+    [Header("Erosion Settings")]
+    
+    const int MAX_RANGE = 10;
+    [Range(1, MAX_RANGE)]
+    public int range = 1;
+
+    const int MAX_STATES = 20;
+    [Range(0, MAX_STATES)]
+    public int nstates = 3;
+
+    // ------------------------------
+    // Global Parameters
+    // ------------------------------
+    [Header("Setup")]
+
+    [Range(0, 2048)]
+    public int rez = 8;
+
+    [Range(0, 50)]
+    public int stepsPerFrame = 0;
+
+    [Range(1, 50)]
+    public int stepMod = 1;
+
+    public ComputeShader cs;
+
+    public Material readMat;
+    public Material writeMat;
+    public Material outMat;
+    
+    private RenderTexture outTex;
+    private RenderTexture readTex;
+    private RenderTexture writeTex;
+
+    private int stepKernel;
+
+    // ------------------------------
+    // Start
+    // ------------------------------
+    private void Start()
+    {
+        Reset();    
+    }
+
+    // ------------------------------
+    // Update
+    // ------------------------------
+    private void Update()
+    {
+        if(Time.frameCount % stepMod == 0)
+        {
+            for (int i = 0; i < stepsPerFrame; i++)
+            {
+                Step();
+            }
+        }
+    }
+
+    // ------------------------------
+    // Reset
+    // ------------------------------
+    [Button]
+    private void Reset()
+    {
+        readTex = CreateTexture(RenderTextureFormat.ARGBFloat);
+        writeTex = CreateTexture(RenderTextureFormat.ARGBFloat);
+        outTex = CreateTexture(RenderTextureFormat.ARGBFloat);
+
+        stepKernel = cs.FindKernel("StepKernel");
+
+        GPUResetKernel();
+    }
+
+    private void GPUResetKernel()
+    {
+        int k = cs.FindKernel("ResetKernel");
+        cs.SetTexture(k, "writeTex", writeTex);
+
+        cs.SetInt("range", range);
+        cs.SetInt("nstates", nstates);
+
+        cs.SetInt("rez", rez);
+        cs.Dispatch(k, rez / 8, rez / 8, 1);
+        SwapTex();
+    }
+
+    // ------------------------------
+    // Step
+    // ------------------------------
+    [Button]
+    public void Step()
+    {
+        cs.SetTexture(stepKernel, "readTex", readTex);
+        cs.SetTexture(stepKernel, "writeTex", writeTex);
+        cs.SetTexture(stepKernel, "outTex", outTex);
+
+        cs.Dispatch(stepKernel, rez / 8, rez / 8, 1);
+
+        SwapTex();
+
+        writeMat.SetTexture("_UnlitColorMap", readTex);
+        readMat.SetTexture("_UnlitColorMap", writeTex);
+        outMat.SetTexture("_UnlitColorMap", outTex);
+    }
+    
+    // ------------------------------
+    // Helper Functions
+    // ------------------------------
+    private void SwapTex()
+    {
+        RenderTexture tmp = readTex;
+        readTex = writeTex;
+        writeTex = tmp;
+    }
+    
+    protected RenderTexture CreateTexture(RenderTextureFormat format)
+    {
+        RenderTexture texture = new RenderTexture(rez, rez, 1, format);
+        texture.enableRandomWrite = true;
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Repeat;
+        texture.useMipMap = false;
+        texture.Create();
+        return texture;
+    }
+}
