b02088ab97e9bceab7ea3ecbe94aae4d4c0fcc1f
Kit MacAllister <kmacallister@secondstory.com>
2020-03-23 23:36:05 -0700

    Revert "3D CCA is working beautifully"
    This reverts commit 05ff46b7efba3605bc1e2da726991809f6a6bb70.

--- Assets/TexInstancer/turntable.cs
+++ Assets/TexInstancer/turntable.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class turntable : MonoBehaviour
 {
-    public Vector3 rotationAngle;
+    public float rotationAngle = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newRotaiton = rotationAngle * Time.time;
-        transform.rotation = Quaternion.Euler(newRotaiton.x, newRotaiton.y, newRotaiton.z);
+        float currenZrotation = transform.rotation.eulerAngles.y;
+        float newRotaiton = rotationAngle * Time.time;
+        transform.rotation = Quaternion.Euler(0, newRotaiton, 0);
     }
 }
